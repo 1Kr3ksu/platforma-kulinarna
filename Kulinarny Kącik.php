7c1989609f3442a3ef1fc7deeda07ed107916b22
1Kr3ksu <kacperbielawski33@gmail.com>
2024-03-13 13:32:57 +0100

    koniec zajec 13.03.2024

---
+++ Kulinarny Kącik.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style.css">
+    <title>Kulinarny-kącik</title>
+</head>
+
+<body>
+    </form>
+
+    <header>
+        <h1>Witaj w Kulinarnym Kąciku</h1>
+    </header>
+    <section class="main">
+        <h2>Tutaj znajdziesz najlepsze przepisy, które przyjdą ci do głowy!</h2>
+        <nav>
+            <a href="Przepisy dnia.html">Przepisy dnia</a>
+            <a href="Najnowsze Pzepisy.html">Najnowsze przepisy</a>
+
+            <form action="/szukaj" method="get">
+                <input type="text" name="query" placeholder="Wyszukaj przepis...">
+                <button type="submit">Szukaj</button>
+            </form>
+
+        </nav>
+
+
+
+
+        <section class="recipe-list">
+            <h3>Dostępne przepisy</h3>
+            <li><a href="Spaghetti.html">Spaghetti</a></li>
+            <li><a href="lasagne.html">Lasagne</a></li>
+            <li><a href="Tiramisu.html">Tiramisu</a></li>
+            <li><a href="Przepisy dnia.html">Naleśniki</a>
+        </section>
+
+        <footer>
+            <p>&copy; 2024 Kacper Bielawski | Wszelkie Prawa Zastrzeżone | Projekt blog kulinarny</p>
+            <!-- Dodaj dodatkowe linki w stopce, jeśli są potrzebne -->
+            <!-- <a href="#">Link 1</a> -->
+            <!-- <a href="#">Link 2</a> -->
+        </footer>
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "platforma_kulinarna";
+        
+        $conn = new mysqli($servername, $username, $password , $dbname);
+        if ($conn->connect_error) {
+            die("Połączenie nieudane: " . $conn->connect_error);
+        }
+        
+         ?>
+</body>
+
+</html>
