a30f74c039c0522d3b5ea0099c53f83fdeb25578
1Kr3ksu <kacperbielawski33@gmail.com>
2024-03-20 13:12:37 +0100

    zmiana 2.03.24 13:12

--- Najnowsze Pzepisy.php
+++ Najnowsze Pzepisy.php
@@ -13,9 +13,9 @@
         <h1>Tutaj znajdują się najnowsze przepisy! </h1>
     </header>
     <form action="/szukaj" method="get">
-                <input type="text" name="query" placeholder="Wyszukaj przepis...">
-                <button type="submit">Szukaj</button>
-            </form>
+        <input type="text" name="query" placeholder="Wyszukaj przepis...">
+        <button type="submit">Szukaj</button>
+    </form>
     <a href="Kulinarny Kącik.html" class="back-button">Powrót </a>
     <section class="Main">
 
@@ -23,6 +23,28 @@
     <section class="how-to">
 
     </section>
+
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "platforma_kulinarna";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Połączenie nieudane: " . $conn->connect_error);
+    }
+    $sql = "SELECT * FROM przepisy";
+
+    $result = $conn->query($sql);
+    echo "<ol>";
+    while ($row = $result->fetch_assoc()) {
+
+        echo "<li>" . $row["tytul"] . "</li>";
+    }
+    echo "</ol>";
+    $conn->close();
+    ?>
 </body>
 
 </html>
--- submit.php
+++ submit.php
@@ -1,25 +1,29 @@
 
 
 <?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "platforma_kulinarna";
-        
-        $conn = new mysqli($servername, $username, $password , $dbname);
-        if ($conn->connect_error) {
-            die("Połączenie nieudane: " . $conn->connect_error);
-        } else {
-            echo "Połączenie_udane!";
-        }
-        $conn->close();
-         
-// pobranie danych z formularza
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "platforma_kulinarna";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Połączenie nieudane: " . $conn->connect_error);
+} else {
+}
+
+
+
 $title = $_POST['title'];
 $description = $_POST['description'];
-// przygotowanie kwerendy
 
-// wykonanie kwerendy
+$sql = "INSERT INTO przepisy(tytul, opis) VALUES ('$title', '$description')";
 
-// zamknięcie połączenia z db
+if ($conn->query($sql) === TRUE) {
+    echo "Gratulacje! Dodałeś przepis!";
+} else {
+    echo "Coś poszło nie tak!";
+}
+
+$conn->close();
 ?>
